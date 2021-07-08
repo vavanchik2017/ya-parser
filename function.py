4c98d37e80edf22d07f4fddf197fbfde1dfc4b99
Vladimir <s3mpau@gmail.com>
2021-07-08 15:30:24 +0300

    Исправлена выдача дубликатов

--- function.py
+++ function.py
@@ -1,14 +1,11 @@
-import requests
 import bs4
 import re
-import lxml
 
-#url = "https://market.yandex.ru/catalog--smartfony-v-volzhskom/54726/list?cpa=0&hid=91491&track=pieces&onstock=1&page=5&local-offers-first=0"
 pattern =  re.compile('/product--.*track=srchbtn.*')
 links = []
 with open("index.html", "r") as file:
     content = file.read()
-soup = bs4.BeautifulSoup(content,"lxml")
+soup = bs4.BeautifulSoup(content,"html.parser")
 for titles in soup.findAll('a', href=True):
     a = re.findall(pattern, str(titles['href']))
     links.append(a)
