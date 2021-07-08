cfffc47a77f9434c4ba3b490edf5cba527beee1c
Vladimir <s3mpau@gmail.com>
2021-07-08 13:54:03 +0300

    Спарсил и отфильтровал ссылки

--- function.py
+++ function.py
@@ -9,11 +9,9 @@ links = []
 with open("index.html", "r") as file:
     content = file.read()
 soup = bs4.BeautifulSoup(content,"lxml")
-#sel = soup.select("div.d0N9PZYfeg a")
 for titles in soup.findAll('a', href=True):
     a = re.findall(pattern, str(titles['href']))
     links.append(a)
 templist = list(filter(None, links))
-
 for x in templist:
     print (x)
