95907be3fc704df79a192a961cb7c20d30c522b3
Vladimir <s3mpau@gmail.com>
2021-07-08 13:23:20 +0300

    Выводятся только ссылки на смартфоны

--- function.py
+++ function.py
@@ -3,12 +3,12 @@ import bs4
 import re
 
 #url = "https://market.yandex.ru/catalog--smartfony-v-volzhskom/54726/list?cpa=0&hid=91491&track=pieces&onstock=1&page=5&local-offers-first=0"
-pattern =  re.compile('href="/product--')
+pattern =  re.compile('/product--.*')
 
 with open("index.html", "r") as file:
     content = file.read()
 soup = bs4.BeautifulSoup(content,"html.parser")
 #sel = soup.select("div.d0N9PZYfeg a")
 for titles in soup.findAll('a', href=True):
-    print(titles['href'])
-    #print(re.findall(pattern, str(titles)))
+    #print(titles['href'])
+    print(re.findall(pattern, str(titles['href'])))
