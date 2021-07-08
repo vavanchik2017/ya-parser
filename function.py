333a00e36a4042dc76e6191e1a5fb2a91c83367c
Vladimir <s3mpau@gmail.com>
2021-07-08 04:19:30 +0300

    Спарсил все ссылки со страницы со смартфонами

---
+++ function.py
@@ -0,0 +1,14 @@
+import requests
+import bs4
+import re
+
+#url = "https://market.yandex.ru/catalog--smartfony-v-volzhskom/54726/list?cpa=0&hid=91491&track=pieces&onstock=1&page=5&local-offers-first=0"
+pattern =  re.compile('href="/product--')
+
+with open("index.html", "r") as file:
+    content = file.read()
+soup = bs4.BeautifulSoup(content,"html.parser")
+#sel = soup.select("div.d0N9PZYfeg a")
+for titles in soup.findAll('a', href=True):
+    print(i, titles['href'])
+    #print(re.findall(pattern, str(titles)))
