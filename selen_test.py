2a117db761e746d509713459d3480734382f4b1a
Vladimir <s3mpau@gmail.com>
2021-07-19 20:48:59 +0300

    Пофиксил баг и изменил вывод

--- selen_test.py
+++ selen_test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import bs4
 import re
 
@@ -11,23 +12,29 @@ pattern =  re.compile('/product--.*nid.*track=srchlink.*')
 links = []
 
 def parser(content):
-	f = open ('links.txt', 'w')
+	f = open ('links1.txt', 'w')
 	soup = bs4.BeautifulSoup(content,"html.parser")
 	for titles in soup.findAll('a', href=True):
 		a = re.findall(pattern, str(titles['href']))
 		links.append(a)
 	templist = list(filter(None, links))
-	f.writelines(["%s\n" % x for x in templist])
+	for x in templist:
+		f.write("market.yandex.ru"+x[0]+"%s\n")	
 
 driver = webdriver.Firefox()
 driver.get(url)
 try:
-    element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Вперёд"))
-    )
+	element = WebDriverWait(driver, 20).until(
+		EC.presence_of_element_located((By.LINK_TEXT, "Вперёд"))
+	)
+
 finally:
 	while (driver.find_element_by_link_text('Вперёд')):
+		try:
 			html_source = driver.page_source
 			parser(html_source)
 			button = driver.find_element_by_link_text('Вперёд')
 			button.click()
+		except NoSuchElementException:
+			html_source = driver.page_source
+			parser(html_source)
