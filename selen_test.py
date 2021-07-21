a5f0216f7edddf9bfcc6c6e07fc7a23447dcbe4d
Vladimir <s3mpau@gmail.com>
2021-07-22 02:39:51 +0300

    Полученные данные добавляются в БД

--- selen_test.py
+++ selen_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -6,23 +7,52 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import bs4
 import re
+import sqlite3
+from datetime import datetime
+
+time = str(datetime.now().time())
+name = "parse_"+time+".db"
+
+db = open(name, "w+")
+
+try:
+    sqlite_connection = sqlite3.connect(name)
+    sqlite_create_table_query = '''CREATE TABLE orders (
+                                id INTEGER PRIMARY KEY,
+                                links TEXT NOT NULL);'''
+
+    cursor = sqlite_connection.cursor()
+    print("База данных подключена к SQL")
+    cursor.execute(sqlite_create_table_query)
+    sqlite_connection.commit()
+    print("Таблица SQLite создана")
+    sqlite_connection.commit()
+    #cursor.close()
+
+except sqlite3.Error as error:
+    print("Ошибка при подключении к sqlite", error)
+
 
 url = "https://market.yandex.ru/catalog--umnye-chasy-i-braslety/56034/list?hid=10498025"
 pattern =  re.compile('/product--.*nid.*track=srchlink.*')
-links = []
 
 def parser(content):
-	f = open ('links1.txt', 'w')
+	links = []
+	templist=[]		
+	f = open ('links.txt', 'w')
 	soup = bs4.BeautifulSoup(content,"html.parser")
 	for titles in soup.findAll('a', href=True):
 		a = re.findall(pattern, str(titles['href']))
 		links.append(a)
 	templist = list(filter(None, links))
 	for x in templist:
-		f.write("market.yandex.ru"+x[0]+"%s\n")	
+		cursor.execute("INSERT INTO orders (links) VALUES (?)",("market.yandex.ru"+x[0], ))
+		sqlite_connection.commit()
 
+	
 driver = webdriver.Firefox()
 driver.get(url)
+
 try:
 	element = WebDriverWait(driver, 20).until(
 		EC.presence_of_element_located((By.LINK_TEXT, "Вперёд"))
@@ -38,3 +68,6 @@ finally:
 		except NoSuchElementException:
 			html_source = driver.page_source
 			parser(html_source)
+
+cursor.close()
+
